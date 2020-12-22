a8f0c8952de00e1d858f25b0d9fd740be3240217
eliaskrtz <eliaskrautz@gmail.com>
2020-12-22 21:29:08 +0100

    Fehlerbehebung

--- WDarrowRight.java
+++ WDarrowRight.java
@@ -6,23 +6,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-<<<<<<< HEAD:arrowRight.java
-public class arrowRight extends wardrobe_object
-=======
-public class WDarrowRight extends wardrobe_objects
->>>>>>> 2188ebef968d5e4248a378632a80ec10955275b2:WDarrowRight.java
+
+
+public class WDarrowRight extends wardrobe_object
+
 {
     /**
      * Act - do whatever the arrowRight wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-<<<<<<< HEAD:arrowRight.java
-    public int c = 0;
-    public arrowRight(int hoehe, int breite) {
-=======
     
+    int c = 0;
     public WDarrowRight(int hoehe, int breite) {
->>>>>>> 2188ebef968d5e4248a378632a80ec10955275b2:WDarrowRight.java
         setImage("/mainChar/clothMenuTexts/arrowRight.png");
         GreenfootImage arRight = getImage();
         arRight.scale(hoehe, breite);
